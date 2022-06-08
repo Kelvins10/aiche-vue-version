3d57f4f724e18c13995cda45b78ddfcfe86d5823
BJzero10 <69209829+BJzero10@users.noreply.github.com>
2022-06-08 12:45:29 -0500

    hombe-page

---
+++ src/components/home/AddressComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    class="xl:px-32 lg:px-20 md:px-14 p-8 md:py-10 py-8 bg-white flex md:flex-row md:content-between items-center flex-col justify-center"
+  >
+    <div class="md:w-1/2 text-center px-10 w-full">
+      <h2 class="mb-3 text-3xl font-bold text-[#000066]">
+        ¿Dónde se va a realizar?
+      </h2>
+      <p class="font-normal text-justify mb-3 text-black">
+        Este 2022, la Universidad anfitriona será la Universidad de Ingeniería y
+        Tecnologia (UTEC).
+      </p>
+      <img
+        class="w-full"
+        src="../../assets/home/UTEC_referencial.jpg"
+        alt="UTEC"
+      />
+    </div>
+    <div class="md:w-1/2 w-full px-10 mt-3 md:mt-0">
+      <iframe
+        class="w-full lg:h-[450px] md:h-96 h-80"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.662845836132!2d-77.02432978518651!3d-12.13520539140849!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105b7fcaea4a133%3A0xb86fbddb0dc89e2e!2sUniversidad%20de%20Ingenier%C3%ADa%20y%20Tecnolog%C3%ADa%20-%20UTEC!5e0!3m2!1ses!2spe!4v1652216378588!5m2!1ses!2spe"
+        style="border: 0"
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,11 +1,15 @@
 <template>
   <banner-component />
+  <about-component />
+  <address-component />
 </template>
 
 <script>
 import BannerComponent from "@/components/home/BannerComponent.vue";
+import AboutComponent from "@/components/home/AboutComponent.vue";
+import AddressComponent from "@/components/home/AddressComponent.vue";
 export default {
-  components: { BannerComponent },
+  components: { BannerComponent, AboutComponent, AddressComponent },
 };
 </script>
 

2ee5e2b0e980d3c5073b124a3a39c6f9bc453f68
Kelvins10 <sullonsilvakelvinjoel@gmail.com>
2022-06-08 22:39:35 -0500

    Protocolos

---
+++ src/components/protocolos/MedidasPrevencion.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="xl:px-32 lg:px-20 md:px-14 px-10 bg-white">
+    <div class="bg-white pt-5 text-center">
+      <h2 class="text-3xl font-bold text-[#006]">Medidas de Prevención</h2>
+      <h3 class="text-black text-lg pt-5 text-center pb-5">
+        Si deseas participar de PROCESA 2022, debes tomar en cuenta las
+        siguientes recomendaciones.
+      </h3>
+    </div>
+    <div class="pb-[10px]">
+      <div class="collapse rounded-xl text-center border border-[#006]">
+        <input type="checkbox" class="peer" />
+        <div
+          class="collapse-title bg-white font-bold text-black text-lg peer-checked:bg-white peer-checked:text-black peer-checked:font-bold"
+        >
+          Ingreso al Perú
+        </div>
+        <div
+          class="collapse-content bg-white text-white peer-checked:bg-white peer-checked:text-black peer-checked:text-justify"
+        >
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+            esse quaerat qui cupiditate sed, possimus, ab asperiores ad
+            molestias nesciunt sequi alias. Ullam officia facere temporibus rem
+            blanditiis quas magni.
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="pb-[10px]">
+      <div class="collapse rounded-xl text-center border border-[#006]">
+        <input type="checkbox" class="peer" />
+        <div
+          class="collapse-title bg-white font-bold text-black text-lg peer-checked:bg-white peer-checked:text-black peer-checked:font-bold"
+        >
+          Ingreso al Campus de UTEC
+        </div>
+        <div
+          class="collapse-content bg-white text-white peer-checked:bg-white peer-checked:text-black peer-checked:text-justify"
+        >
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+            esse quaerat qui cupiditate sed, possimus, ab asperiores ad
+            molestias nesciunt sequi alias. Ullam officia facere temporibus rem
+            blanditiis quas magni.
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="pb-[10px]">
+      <div class="collapse rounded-xl text-center border border-[#006]">
+        <input type="checkbox" class="peer" />
+        <div
+          class="collapse-title bg-white font-bold text-black text-lg peer-checked:bg-white peer-checked:text-black peer-checked:font-bold"
+        >
+          Normas de Bioseguridad
+        </div>
+        <div
+          class="collapse-content bg-white text-white peer-checked:bg-white peer-checked:text-black peer-checked:text-justify"
+        >
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+            esse quaerat qui cupiditate sed, possimus, ab asperiores ad
+            molestias nesciunt sequi alias. Ullam officia facere temporibus rem
+            blanditiis quas magni.
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="pb-[10px]">
+      <div class="collapse rounded-xl text-center border border-[#006]">
+        <input type="checkbox" class="peer" />
+        <div
+          class="collapse-title bg-white font-bold text-black text-lg peer-checked:bg-white peer-checked:text-black peer-checked:font-bold"
+        >
+          Consejos
+        </div>
+        <div
+          class="collapse-content bg-white text-white peer-checked:bg-white peer-checked:text-black peer-checked:text-justify"
+        >
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+            esse quaerat qui cupiditate sed, possimus, ab asperiores ad
+            molestias nesciunt sequi alias. Ullam officia facere temporibus rem
+            blanditiis quas magni.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/views/ProtocolosPage.vue
+++ src/views/ProtocolosPage.vue
@@ -1,9 +1,16 @@
 <template>
-  <h1>Protocolos Page</h1>
+  <div class="Banner">
+    <BannerProtocolos />
+  </div>
+  <div class="MedidasPrevencion">
+    <MedidasPrevencion />
+  </div>
 </template>
 
 <script>
-export default {};
+import BannerProtocolos from "@/components/protocolos/BannerProtocolos.vue";
+import MedidasPrevencion from "../components/protocolos/MedidasPrevencion.vue";
+export default { components: { BannerProtocolos, MedidasPrevencion } };
 </script>
 
 <style></style>

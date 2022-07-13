3f5093fcd94f18d9fb057415d87fa663dc1d03c6
BJzero10 <69209829+BJzero10@users.noreply.github.com>
2022-07-12 22:33:25 -0500

    actualización inscripciones

--- src/components/inscripciones/TarifasComponent.vue
+++ src/components/inscripciones/TarifasComponent.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="xl:px-32 lg:px-20 md:px-14 px-10 bg-white">
-    <div class="bg-white pt-5 text-center">
+    <div class="bg-white pt-5 text-center pb-5">
       <h2 class="md:text-3xl sm:text-2xl text-xl font-bold text-[#006] pb-3">
         Regístrate
       </h2>
-      <h3 class="text-black md:text-lg sm:text-base text-smpt-5 text-center">
+      <!-- <h3 class="text-black md:text-lg sm:text-base text-smpt-5 text-center">
         Si quieres estar en el PROCESA 2022 deberás completar tu registro
         haciendo click en
         <a
@@ -15,8 +15,109 @@
         >
         y luego realizar el pago según las tarifas generales presentadas a
         continuación:
-      </h3>
+      </h3> -->
+      <h4 class="text-black md:text-lg sm:text-base text-smpt-5 text-center">
+        Si quieres estar en el PROCESA 2022 deberás completar los siguientes
+        pasos según si eres participante nacional o internacional:
+      </h4>
     </div>
+    <div class="bg-white pt-5 text-justify pb-5">
+      <h3 class="md:text-xl sm:text-lg text-sm font-bold text-black pb-3">
+        Nacionales:
+      </h3>
+      <ol class="pl-4">
+        <li class="text-black md:text-base text-sm pb-2">
+          <b>1)</b> Completar el formulario de registro al PROCESA 2022, este
+          paso es imprescindible (
+          <a
+            href="https://forms.gle/bRkZ95FdkJrAv26KA"
+            target="blank"
+            class="link link-accent text-[#00BFFF]"
+            >https://forms.gle/bRkZ95FdkJrAv26KA</a
+          >).
+        </li>
+        <li class="text-black md:text-base text-sm pb-2">
+          <b>2)</b> Realizar el pago de la inscripción a partir de las
+          <b>9:00 am</b> del día siguiente posterior al registro, el pago lo
+          podrás realizar a través de la banca móvil, banca por internet o
+          ventanilla de los bancos BCP, Interbank, Scotiabank y BBVA.
+        </li>
+        <li class="text-black md:text-base text-sm pb-2">
+          <b>3)</b> Para el caso de banca móvil y banca por internet deberás
+          seleccionar “Pago de Servicios”, luego buscar “Utec” y seleccionar
+          “Utec Derechos”. Te pedirá ingresar tu N° de DNI con el que te
+          registraste en el formulario (Ejemplo:
+          <a
+            href="https://youtu.be/2gmCaqJXrR8"
+            target="blank"
+            class="link link-accent text-[#00BFFF]"
+            >https://youtu.be/2gmCaqJXrR8</a
+          >).
+        </li>
+        <li class="text-black md:text-base text-sm pb-2">
+          <b>4)</b> Para el caso de ventanilla deberás mencionar el pago a Utec
+          > Utec Derechos y proporcionar tu N° de DNI con el que te registraste
+          en el formulario.
+        </li>
+        <li class="text-black md:text-base text-sm pb-2">
+          <b>5)</b> Enviar el comprobante de pago a
+          <a
+            class="text-[15px] md:text-lg sm:text-base text-sm text-[#00BFFF]"
+            href="mailto:procesa@utec.edu.pe"
+            >procesa@utec.edu.pe</a
+          >
+          con el asunto “Pago PROCESA 2022” adjuntando tus nombres y apellidos
+          (Importante que se note el código o número de operación).
+        </li>
+      </ol>
+    </div>
+    <div class="bg-white pt-5 text-justify pb-5">
+      <h3 class="md:text-xl sm:text-lg text-sm font-bold text-black pb-3">
+        Internacionales:
+      </h3>
+      <ol class="pl-4">
+        <li class="text-black md:text-base text-sm pb-2">
+          <b>1)</b> Completar el formulario de registro al PROCESA 2022, este
+          paso es imprescindible (
+          <a
+            href="https://forms.gle/bRkZ95FdkJrAv26KA"
+            target="blank"
+            class="link link-accent text-[#00BFFF]"
+            >https://forms.gle/bRkZ95FdkJrAv26KA</a
+          >).
+        </li>
+        <li class="text-black md:text-base text-sm pb-2">
+          <b>2)</b> Realizar el pago de la inscripción en cualquier banco de tu
+          país proporcionando los siguientes datos:
+          <div class="flex justify-center pt-2 w-full pb-3">
+            <img
+              class="shadow-md shadow-[#231f20] rounded-xl border border-[#231f20] w-full"
+              src="../../assets/inscripciones/datos_pago.png"
+            />
+          </div>
+          <h3 class="text-black md:text-base text-sm pb-2 text-justify">
+            <i
+              >(Es importante que realices solo el pago de los 55 dólares
+              exactos dado que la organización asumirá el descuento por la
+              transacción internacional).</i
+            >
+          </h3>
+        </li>
+        <li class="text-black md:text-base text-sm pb-2">
+          <b>3)</b> Enviar el comprobante de pago a
+          <a
+            class="text-[15px] md:text-lg sm:text-base text-sm text-[#00BFFF]"
+            href="mailto:procesa@utec.edu.pe"
+            >procesa@utec.edu.pe</a
+          >
+          con el asunto “Pago PROCESA 2022” adjuntando tus nombres y apellidos
+          (Importante que se note el código o número de operación).
+        </li>
+      </ol>
+    </div>
+    <h3 class="md:text-xl sm:text-lg text-sm font-bold text-black pb-3">
+      Tarifario:
+    </h3>
     <div class="flex justify-center pt-2 w-full pb-10">
       <img
         class="shadow-md shadow-[#231f20] rounded-xl border border-[#231f20] w-full"
@@ -33,6 +134,9 @@
       <span class="font-bold text-[#006]">
         último día para el pago de inscripción será el 31 de julio.
       </span>
+      Y además, de que si se desea una factura a nombre de la universidad el
+      pago deberá ser teniendo una cuenta a tu nombre en los bancos donde se
+      realiza la transacción.
     </h3>
   </div>
 </template>
